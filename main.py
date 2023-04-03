0f3ed6582f831d7630f3c23fdf4b356249d47ab6
emanmozaffar <emanmozaffar@Emans-MacBook-Pro.local>
2023-04-03 12:39:56 -0400

    Update main script to catch potential input errors from user. Fix infinite loops and bad context.

--- main.py
+++ main.py
@@ -22,37 +22,52 @@ def main():
     -------
     None
     """
-    goal = input("Enter your goal: ")
-    timeline_str = input("Enter the timeline to complete your goal (in days): ")
-    details = input("Enter some details about what you want to accomplish: ")
+    while True:
+        goal = input("Enter your goal: ")
+        timeline_str = input("Enter the timeline to complete your goal (in days): ")
+        details = input("Enter some details about what you want to accomplish, and how: ")
 
-    try:
-        timeline = int(timeline_str)
-    except ValueError:
-        print("Invalid input. Please enter a number for the timeline.")
-        return
+        try:
+            timeline = int(timeline_str)
+        except ValueError:
+            print("Invalid input. Please enter a number for the timeline.")
+            continue
 
-    start_date = datetime.datetime.now()
-    prompt = (
-        f"Generate a detailed action plan with steps and due dates in a list format for achieving the "
-        f"following goal in {timeline} days: '{goal}'. Details: {details}. Start date: {start_date:%Y-%m-%d}. "
-        f"Please provide the action plan as a list of dictionaries, where each dictionary represents a step "
-        f"and contains keys and values for the following fields: 'Subject', 'Start Date', 'Start Time', 'End Date', "
-        f"'End Time', 'All Day Event', 'Description', 'Location', 'Private'. This will be used to create Google "
-        f"Calendar events after the list of dictionaries is written to a CSV file so it can be imported by the user."
-    )
+        start_date = datetime.datetime.now()
+        prompt = (
+            f"Generate a detailed action plan with steps and due dates in a list format for achieving the "
+            f"following goal in {timeline} days: '{goal}'. Details: {details}. Start date: {start_date:%Y-%m-%d}. "
+            f"Please provide the action plan as a list of dictionaries, where each dictionary represents a step "
+            f"and contains keys and values for the following fields: 'Subject', 'Start Date', 'Start Time', 'End Date', "
+            f"'End Time', 'All Day Event', 'Description', 'Location', 'Private'. This will be used to create Google "
+            f"Calendar events after the list of dictionaries is written to a CSV file so it can be imported by the user."
+        )
 
-    action_plan_text = generate_action_plan(prompt)
-    action_plan = eval(action_plan_text)
-    print("\nAction Plan:")
-    for step in action_plan:
-        print(f"{step['Subject']} ({step['Start Date']} - {step['End Date']})")
+        action_plan_text = generate_action_plan(prompt)
 
-    save_option = input("\nDo you want to save the action plan as a CSV file for Google Calendar? (yes/no): ").lower()
-    if save_option == "yes":
-        file_name = input("Enter the file name (without extension): ")
-        save_to_csv(action_plan, file_name + ".csv")
-        print(f"Action plan saved to {file_name}.csv. You can import this file into your Google Calendar directly. Good luck!")
+        try:
+            action_plan = eval(action_plan_text)
+        except:
+            print("Sorry, your inputs did not contain the right information to create a proper action plan. Please try again!")
+            continue
+
+        print("\nAction Plan:")
+        for step in action_plan:
+            print(f"{step['Subject']} ({step['Start Date']} - {step['End Date']})")
+
+        while True:
+            save_option = input("\nDo you want to save the action plan as a CSV file for Google Calendar? (yes/no): ").lower()
+            if save_option == "yes":
+                file_name = input("Enter the file name (without extension): ")
+                save_to_csv(action_plan, file_name + ".csv")
+                print(f"Plan saved to {file_name}.csv. You can import this file into your Google Calendar and adjust as needed. Good luck!")
+                return
+            elif save_option == "no":
+                print("Bye!")
+                return
+            else:
+                print("I didn't understand your input. Please try again.")
+                continue
 
 if __name__ == "__main__":
     main()
